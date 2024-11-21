4dee0760f4b4b7451de8f5d22d3abeffcafc455c
Chaitanya Davuluri <150474331+MrChaitu111@users.noreply.github.com>
2024-11-22 01:00:05 +0530

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
 <nk>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>index</title>
+    <title>PHP_CURD_OPERATIONS</title>
+    <link rel="icon" href="best.png">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous"> 
 </head>
 <body>
